1d13335cc311f55f02584c75fc778bfd9d925abb
viktekbl <77540645+Vikek@users.noreply.github.com>
2024-11-07 21:27:35 +0100

    homepage and categories routes setup

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,6 +2,12 @@ const { Router } = require("express");
 const usersController = require("../controllers/usersController");
 const usersRouter = Router();
 
+usersRouter.get("/", (req, res) => {
+    res.render("index");
+});
 
+usersRouter.get("/categories", (req, res) => {
+    res.render("categories");
+});
 
 module.exports = usersRouter;
